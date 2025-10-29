27385f96586a5469cfb2a7001a52c134d2ff7573
dima.artemov <artemov.dmitriy07@gmail.com>
2025-10-29 21:20:41 +0300

    fix/feat: update dev & build scripts

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,23 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   root: 'ui',
   plugins: [react()],
+  esbuild: { target: 'es2018' },
   build: {
+    target: 'es2018',
     outDir: '../dist',
     emptyOutDir: false,
     rollupOptions: {
       input: 'ui/index.html',
       output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
         entryFileNames: 'ui.[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
+        chunkFileNames: 'chunks/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
+    cssCodeSplit: true,
+    chunkSizeWarningLimit: 1500 // чтобы не видеть варнинг в dev
   },
-  server: {
-    port: 5173,
-    strictPort: true
-  }
 })
